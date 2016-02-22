07772c642c68f4b906c8ea9b2d767fb1b910ffdb
popigg <popigg@gmail.com>
2016-02-22 10:49:26 +0100

    serve different files when prod or dev

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var koa     = require('koa');
 var views   = require('co-views');
+var serve   = require('koa-static');
 var app     = koa();
 
-var render = views(__dirname + '/', { map: { html: 'swig' }});
+if (process.env.NODE_ENV == 'development') {
+  var render = views(__dirname + '/', { map: { html: 'swig' }});
+} else {
+  var render = views(__dirname + '/public', { map: { html: 'swig' }});
+}
+
+app.use(serve(__dirname + '/build'));
 
 app.use(function *(){
     this.body = yield render('index');
