97acf2058212aecd3a81ed9a43fc6b8741ad250c
popigg <popigg@gmail.com>
2016-05-13 00:28:28 +0200

    rework sidebar

--- app/components/main/components/sidebar/sidebar.js
+++ app/components/main/components/sidebar/sidebar.js
@@ -4,10 +4,9 @@ import { Link } from 'react-router';
 class Sidebar extends React.Component {
     render() {
         return (
-            <aside id="sidebar-wrapper">
-                <nav className="sidebar-nav">
+            <aside id="sidebar" className="col-xs-4">
+                <nav>
                     <ul>
-                        <li><Link to="/about.me">about.me</Link></li>
                         <li><Link to="/resu.me">resu.me</Link></li>
                         <li><Link to="/project.me">project.me</Link></li>
                         <li><Link to="/inspire.me">inspire.me</Link></li>
