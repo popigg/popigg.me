162bf68c07e7413dca1d833d01bf29d359f03697
popigg <popigg@gmail.com>
2016-05-29 18:40:39 +0200

    distribute space + update content

--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -4,36 +4,31 @@ import { Link } from 'react-router';
 class Sidebar extends React.Component {
     render() {
         return (
-            <aside className="sidebar col-xs-3">
-                <nav>
+            <aside className="sidebar col-xs-offset-1 col-xs-3 center-xs">
+                <nav className="col-xs-10">
                     <ul>
                       <li className="sidebar__listItem sidebar__listItem--noDecoration">
                         <Link to="/">
-                          <span className="sidebar__icon sidebar__icon--inception">&nbsp;</span>
                           Inception
                         </Link>
                       </li>
                       <li className="sidebar__listItem sidebar__listItem--noDecoration">
                         <Link to="/inspire">
-                          <span className="sidebar__icon sidebar__icon--inspire">&nbsp;</span>
-                          What Inspires me
+                          Inspiration
                         </Link>
                       </li>
                       <li className="sidebar__listItem sidebar__listItem--noDecoration">
                         <Link to="/jobs">
-                          <span className="sidebar__icon sidebar__icon--jobs">&nbsp;</span>
                           Pet Projects
                         </Link>
                       </li>
                       <li className="sidebar__listItem sidebar__listItem--noDecoration">
                         <Link to="/read">
-                          <span className="sidebar__icon sidebar__icon--blog">&nbsp;</span>
-                          Stories i like to tell
+                          Stories
                         </Link>
                       </li>
                       <li className="sidebar__listItem sidebar__listItem--noDecoration">
                         <Link to="/contact">
-                          <span className="sidebar__icon sidebar__icon--contact">&nbsp;</span>
                           Find me
                         </Link>
                       </li>
