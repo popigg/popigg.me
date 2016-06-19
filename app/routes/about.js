c19c000c7f567b5fa1f636b044962d94f6affac0
popigg <popigg@gmail.com>
2016-06-20 00:09:09 +0200

    add about page animation

--- app/routes/about.js
+++ app/routes/about.js
@@ -15,35 +15,35 @@ class About extends React.Component {
 
               <div className="col-xs-offset-2 col-xs-8 about__item">
                 <div className="row center-xs about__item--creativity">
-                  <span>creativity</span>
+                  <h3>creativity</h3>
                   <p className="about__item--description">
                     change happens so quickly, find the inspiration to do valuable things.
                   </p>
                 </div>
 
                 <div className="row center-xs about__item--challenge">
-                  <span>challenge</span>
+                  <h3>challenge</h3>
                   <p className="about__item--description">
                     repeated jobs are boring, use challenges to learn something new
                   </p>
                 </div>
 
                 <div className="row center-xs about__item--teamwork">
-                  <span>teamwork</span>
+                  <h3>teamwork</h3>
                   <p className="about__item--description">
                     working with people that complements you makes you better.
                   </p>
                 </div>
 
                 <div className="row center-xs about__item--poc">
-                  <span>p.o.c.</span>
+                  <h3>p.o.c.</h3>
                   <p className="about__item--description">
                     perfect first tries are unusual, get the feeling with a proof of concept.
                   </p>
                 </div>
 
                 <div className="row center-xs about__item--freedom">
-                  <span>freedom</span>
+                  <h3>freedom</h3>
                   <p className="about__item--description">
                     loving my family and my free time make me want to be eficient.
                   </p>
