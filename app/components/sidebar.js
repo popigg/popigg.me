b49df83c886dcac36f9c731d33c90698dd05b18a
popigg <popigg@gmail.com>
2016-06-26 15:48:12 +0200

    make like active only when needed

--- app/components/sidebar.js
+++ app/components/sidebar.js
@@ -1,44 +1,69 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, IndexLink } from 'react-router';
+import TweenMax from 'gsap';
 import circle from '../../public/images/grunge-circle-3.svg';
 
 class Sidebar extends React.Component {
-    render() {
-        return (
-            <aside className="sidebar col-xs-offset-1 col-xs-3 center-xs">
-                <nav className="col-xs-10">
-                    <ul>
-                      <li className="sidebar__listItem sidebar__listItem--noDecoration">
-                        <Link to="/">
-                          Inception
-                        </Link>
-                      </li>
-                      <li className="sidebar__listItem sidebar__listItem--noDecoration">
-                        <Link to="/inspire">
-                          Inspiration
-                        </Link>
-                      </li>
-                      <li className="sidebar__listItem sidebar__listItem--noDecoration">
-                        <Link to="/jobs">
-                          Pet Projects
-                        </Link>
-                      </li>
-                      <li className="sidebar__listItem sidebar__listItem--noDecoration">
-                        <Link to="/read">
-                          Stories
-                        </Link>
-                      </li>
-                      <li className="sidebar__listItem sidebar__listItem--noDecoration">
-                        <Link to="/contact">
-                          Find me
-                        </Link>
-                      </li>
-                    </ul>
-                </nav>
-                <img src={circle} className="sidebar__elem--decoration"/>
-            </aside>
-        )
+
+  twistThrobber() {
+    console.log('ouch!!!');
+    let throbber = document.getElementById('throbber');
+    console.log('Throbber ===>', throbber);
+    if (throbber) {
+      TweenMax.to(throbber,2, {rotation:"-=10_cw"});
     }
+  }
+
+  render() {
+      return (
+        <aside className="sidebar col-xs-3 center-xs">
+          <nav className="col-xs-10">
+            <ul>
+              <li className="sidebar__listItem sidebar__listItem--noDecoration">
+                <IndexLink to="/" className="sidebar__link"
+                  onClick={this.twistThrobber()}
+                  activeClassName="sidebar__link--active"
+                  to="/">
+                  Inception
+                </IndexLink>
+              </li>
+              <li className="sidebar__listItem sidebar__listItem--noDecoration">
+                <Link className="sidebar__link"
+                  onClick={this.twistThrobber()}
+                  activeClassName="sidebar__link--active" to="/about">
+                  About
+                </Link>
+              </li>
+              <li className="sidebar__listItem sidebar__listItem--noDecoration">
+                <Link className="sidebar__link"
+                  activeClassName="sidebar__link--active" to="/inspire">
+                  Inspiration
+                </Link>
+              </li>
+              <li className="sidebar__listItem sidebar__listItem--noDecoration">
+                <Link className="sidebar__link"
+                  activeClassName="sidebar__link--active" to="/jobs">
+                  Pet Projects
+                </Link>
+              </li>
+              <li className="sidebar__listItem sidebar__listItem--noDecoration">
+                <Link className="sidebar__link"
+                  activeClassName="sidebar__link--active" to="/read">
+                  Stories
+                </Link>
+              </li>
+              <li className="sidebar__listItem sidebar__listItem--noDecoration">
+                <Link className="sidebar__link"
+                  activeClassName="sidebar__link--active" to="/contact">
+                  Find me
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          <img id="throbber" src={circle} className="sidebar__elem--decoration"/>
+        </aside>
+      )
+  }
 }
 
 export default Sidebar;
