0d2a47cde569846351cdc321f2b025040299d866
popigg <popigg@gmail.com>
2016-06-26 15:47:59 +0200

    transition group animation

--- app/components/main.js
+++ app/components/main.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactCSSTransitionGroup from 'react-addons-transition-group'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 import Sidebar from './sidebar'
 import About from '../routes/about'
@@ -7,19 +7,24 @@ import About from '../routes/about'
 
 class Main extends React.Component {
     render() {
-        const { pathname } = this.props.location
+      const { pathname } = this.props.location
+      return (
+        <div className="row center-xs">
+          <div className="col-xs-9">
+              {<ReactCSSTransitionGroup
+                transitionName="main__views--transition"
+                transitionAppear={true}
+                transitionEnterTimeout={300}
+                transitionAppearTimeout={300}
+                transitionLeaveTimeout={100}
+                component="div">
+                  {React.cloneElement(this.props.children, { key: pathname })}
+              </ReactCSSTransitionGroup>}
+          </div>
 
-        return (
-            <div className="row">
-              <div className="col-xs-offset-1 col-xs-7 around-xs">
-                  <ReactCSSTransitionGroup component="main" id="wrap">
-                      {React.cloneElement(this.props.children || <About />, { key: pathname })}
-                  </ReactCSSTransitionGroup>
-              </div>
-
-              <Sidebar/>
-            </div>
-        )
+          <Sidebar/>
+        </div>
+      )
     }
 }
 
