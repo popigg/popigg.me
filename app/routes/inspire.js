6e8f3bea50861ab10a10e1596a94f75290904438
popigg <popigg@gmail.com>
2016-07-26 00:03:29 +0200

    play with spacing

--- app/routes/inspire.js
+++ app/routes/inspire.js
@@ -5,7 +5,11 @@ class Inspire extends React.Component {
         return (
             <div>
                 <h1>inspire.me</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In eget est in dolor dignissim tincidunt. Maecenas vitae nibh hendrerit, semper magna vel, blandit sem. Morbi ultricies ac est ac vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec a risus vel felis commodo pulvinar.</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  In eget est in dolor dignissim tincidunt. Maecenas vitae nibh
+                  hendrerit, semper magna vel, blandit sem. Morbi ultricies ac est ac vulputate.
+                  Vestibulum ante ipsum primis in faucibus orci luctus et
+                  ultrices posuere cubilia Curae; Donec a risus vel felis commodo pulvinar.</p>
             </div>
         )
     }
