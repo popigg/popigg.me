37e4d4450c02bec4907084eabc6cabf0480a5850
popigg <popigg@gmail.com>
2016-09-12 17:43:42 +0200

    rework pack location and code styling

--- webpack.config.js
+++ webpack.config.js
@@ -2,52 +2,52 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-    entry: getEntrySources(['./app/index.js']),
-    output: {
-        path: path.join(__dirname),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    devtool: 'eval',
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
+  entry: getEntrySources(['./app/index.js']),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist',
+    filename: 'bundle.js'
+  },
+  devtool: 'eval',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'source-map'
+      }
     ],
-    module: {
-        preLoaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'source-map'
-            }
-        ],
+    loaders: [
+      {
+        test: /\.scss$/,
+        include: /app/,
         loaders: [
-            {
-                test: /\.scss$/,
-                include: /app/,
-                loaders: [
-                    'style',
-                    'css',
-                    'autoprefixer?browsers=last 3 versions',
-                    'sass?outputStyle=expanded'
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg|woff|woff2|eot|ttf)$/i,
-                loaders: [
-                    'url?limit=8192',
-                    'img'
-                ]
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loaders: [
-                    'react-hot',
-                    'babel'
-                ]
-            }
+          'style',
+          'css',
+          'autoprefixer?browsers=last 3 versions',
+          'sass?outputStyle=expanded'
         ]
-    }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|woff|woff2|eot|ttf)$/i,
+        loaders: [
+          'url?limit=8192',
+          'img'
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: [
+          'react-hot',
+          'babel'
+        ]
+      }
+    ]
+  }
 };
 
 function getEntrySources(sources) {
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -3,45 +3,45 @@ var path = require('path');
 module.exports = {
     entry: './app/index.js',
     output: {
-      path: path.join(__dirname),
-      publicPath: '/',
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/dist',
       filename: 'bundle.js'
     },
     devtool: 'eval',
     module: {
-        preLoaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loader: 'source-map'
-            }
-        ],
-        loaders: [
-            {
-                test: /\.scss$/,
-                include: /app/,
-                loaders: [
-                    'style',
-                    'css',
-                    'autoprefixer?browsers=last 3 versions',
-                    'sass?outputStyle=expanded'
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg|woff|woff2|eot|ttf)$/i,
-                loaders: [
-                    'url?limit=8192',
-                    'img'
-                ]
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/,
-                loaders: [
-                    'react-hot',
-                    'babel'
-                ]
-            }
-        ]
-    }
+      preLoaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'source-map'
+        }
+      ],
+      loaders: [
+        {
+          test: /\.scss$/,
+          include: /app/,
+          loaders: [
+            'style',
+            'css',
+            'autoprefixer?browsers=last 3 versions',
+            'sass?outputStyle=expanded'
+          ]
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg|woff|woff2|eot|ttf)$/i,
+          loaders: [
+            'url?limit=8192',
+            'img'
+          ]
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loaders: [
+            'react-hot',
+            'babel'
+          ]
+        }
+      ]
+  }
 };
