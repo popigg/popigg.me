e1f1480c672460fb40af84c2c87c254eb58791b3
popigg <popigg@gmail.com>
2016-09-19 00:33:05 +0200

    tune couple of paragraphs

--- app/routes/home.js
+++ app/routes/home.js
@@ -4,12 +4,11 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-              <div className="home__header">
-                hello!!! my name is <span className="home__header--highlight">Pablo Gomez Guerrero</span>
+              <div className="home__header start-xs">
                 <p>
-                  father, sportman, reader, traveller, thinker and Generalist Developer,
+                  Hello!!! my name is <span className="home__header--highlight">Pablo Gomez Guerrero</span>,
+                  thinker and generalist developer,
                   now at <a target="_blank" href="http://sentisis.com">Sentisis</a>.
-                  <br/>
                 </p>
               </div>
 
@@ -18,20 +17,14 @@ class Home extends React.Component {
                   <div className="col-xs-6 center-xs home__item--creativity">
                     <h3>creativity</h3>
                     <p className="home__item--description">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        In eget est in dolor dignissim tincidunt. Maecenas vitae nibh
-                        hendrerit, semper magna vel,
-                      {/*change happens so quickly, find the inspiration to do valuable things.*/}
-                    </p>
+                      Find the inspiration to solve problems with imagination.
+                     </p>
                   </div>
 
                   <div className="col-xs-6 center-xs home__item--challenge">
                     <h3>challenge</h3>
                     <p className="home__item--description">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        In eget est in dolor dignissim tincidunt. Maecenas vitae nibh
-                        hendrerit, semper magna vel,
-                      {/*repeated jobs are boring, use challenges to learn something new*/}
+                       Using challenges to learn something new, opportunities will always come.
                     </p>
                   </div>
                 </div>
@@ -40,20 +33,15 @@ class Home extends React.Component {
                   <div className="col-xs-6 center-xs home__item--teamwork">
                     <h3>teamwork</h3>
                     <p className="home__item--description">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        In eget est in dolor dignissim tincidunt. Maecenas vitae nibh
-                        hendrerit, semper magna vel,
-                      {/*working with people that complements you makes you better.*/}
+                      Working with people makes me better.
+                      Everybody got its own "magic" to teach.
                     </p>
                   </div>
 
                   <div className="col-xs-6 center-xs home__item--poc">
                     <h3>p.o.c.</h3>
                     <p className="home__item--description">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        In eget est in dolor dignissim tincidunt. Maecenas vitae nibh
-                        hendrerit, semper magna vel,
-                      {/*perfect first tries are unusual, get the feeling with a proof of concept.*/}
+                      Perfect first tries are unusual for me, so i like touching things first.
                     </p>
                   </div>
                 </div>
@@ -62,15 +50,13 @@ class Home extends React.Component {
                   <div className="col-xs-6 col-xs-offset-3 center-xs home__item--freedom">
                     <h3>freedom</h3>
                     <p className="home__item--description">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        In eget est in dolor dignissim tincidunt. Maecenas vitae nibh
-                        hendrerit, semper magna vel,
-                      {/*loving my family and my free time make me want to be eficient.*/}
+                      I love my free time and spend time with family.
+                      Why not work less hours producing the same or more?
                     </p>
                   </div>
                 </div>
               </div>
-              <p className="home__footer">Do you want to read <a target="_blank" href="https://stackoverflow.com/cv/popigg">more</a>?</p>
+              <p className="home__footer start-xs">Do you want to read <a target="_blank" href="https://stackoverflow.com/cv/popigg">more</a>?</p>
             </div>
         )
     }
