d734021dae2b4d1740f9d3dc51edb22c8f5b5c14
popigg <popigg@gmail.com>
2016-09-19 00:33:17 +0200

    left align

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -6,36 +6,38 @@ class Contact extends React.Component {
         return (
             <div>
                 <PageTitle title="contact" overlineColor='pink'/>
-                <p>
-                  <a href="https://github.com/popigg" target="_blank">
-                    <span className="contact__social--github">&nbsp;</span>
-                    <span>popigg</span>
-                  </a>
-                </p>
-                <p>
-                  <a href="https://bitbucket.org/popigg" target="_blank">
-                    <span className="contact__social--bitbucket">&nbsp;</span>
-                    <span>popigg</span>
-                  </a>
-                </p>
-                <p>
-                  <a href="https://twitter.com/popigg" target="_blank">
-                    <span className="contact__social--twitter">&nbsp;</span>
-                    <span>@popigg</span>
-                  </a>
-                </p>
-                <p>
-                  <a href="https://www.linkedin.com/in/pablo-gomez-guerrero-095abb22" target="_blank">
-                    <span className="contact__social--linkedin">&nbsp;</span>
-                    <span className="contact__social--linkedinDescription">Pablo Gomez Guerrero</span>
-                  </a>
-                </p>
-                <p>
-                  <a href="mailto:popigg@gmail.com" target="_top">
-                    <span className="contact__social--email">&nbsp;</span>
-                    <span>popigg[at]gmail.com</span>
-                  </a>
-                </p>
+                <div className="contact__social start-xs">
+                  <p>
+                    <a href="https://github.com/popigg" target="_blank">
+                      <span className="contact__social--github">&nbsp;</span>
+                      <span>popigg</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="https://bitbucket.org/popigg" target="_blank">
+                      <span className="contact__social--bitbucket">&nbsp;</span>
+                      <span>popigg</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="https://twitter.com/popigg" target="_blank">
+                      <span className="contact__social--twitter">&nbsp;</span>
+                      <span>@popigg</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="https://www.linkedin.com/in/pablo-gomez-guerrero-095abb22" target="_blank">
+                      <span className="contact__social--linkedin">&nbsp;</span>
+                      <span className="contact__social--linkedinDescription">Pablo Gomez Guerrero</span>
+                    </a>
+                  </p>
+                  <p>
+                    <a href="mailto:popigg@gmail.com" target="_top">
+                      <span className="contact__social--email">&nbsp;</span>
+                      <span>popigg[at]gmail.com</span>
+                    </a>
+                  </p>
+                </div>
             </div>
         )
     }
