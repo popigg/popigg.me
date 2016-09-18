5a32d4371d25321ef7c0295f4ff58c24cb512cea
popigg <popigg@gmail.com>
2016-09-19 00:32:45 +0200

    finish about description

--- app/routes/about.js
+++ app/routes/about.js
@@ -1,55 +1,54 @@
 import React from 'react';
 import imgSrc from '../../public/images/pencil.jpg';
+import PageTitle from '../components/PageTitle';
 
 class About extends React.Component {
     render() {
         return (
             <div>
-                <img src={imgSrc} className="row col-xs-12 about__image--header" />
-                <h1 className="about__header">about</h1>
-                <div className="about__content">
+                <img src={imgSrc} className="row col-xs-10 about__image--header" />
+                <PageTitle title="about" overlineColor='yellow'/>
+                <div className="about__content start-xs">
+
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Quisque convallis efficitur diam, sit amet laoreet ipsum finibus sed.
-                    Aenean fermentum venenatis eleifend. Praesent ac nisl in eros interdum
-                    ultrices volutpat eu massa.
-                    Nunc tempor diam justo, ut sollicitudin velit rhoncus in.
-                    Etiam blandit quam vel sem varius, in vulputate mauris posuere.
-                    Integer molestie nisl sed scelerisque gravida. Nam varius lacus est,
-                    id scelerisque nibh viverra quis. Sed molestie sapien libero,
-                    nec faucibus ligula condimentum sed. Aliquam sagittis rhoncus
-                    purus vitae condimentum. Donec eget fringilla justo.
-                    Morbi quis quam dapibus, vulputate neque eu, hendrerit arcu.
-                    Phasellus sed ligula malesuada, maximus massa vel, ullamcorper orci.
+                    I like technology very much, and i have freely decided to dedicate
+                    my life to it. Sometimes is a world where you are avocated to change or
+                    mutate your skills, work on your free time, understand complexity
+                    you cannot put in your mind in a language is not you mother tongue.
+                    It is not easy.
+                    Since around 10 years i found my self very motivated and try to build something
+                    to feel proud of.
+                    I want to build things that change and improve in something the world we live.
+                    It makes me feel very positive, happy and glad that technology allows me this.
                   </p>
                   <p>
-                    Duis facilisis odio eu rhoncus efficitur. In cursus rutrum tortor.
-                    Donec risus turpis, facilisis id nisl id, consectetur dictum nisi.
-                    Quisque in magna non elit congue pretium vel eu neque.
-                    Vivamus vitae est justo. Praesent non commodo enim.
-                    Sed porta vulputate ligula, vel euismod nisi accumsan sed.
+                    After sharing experiences with many people,
+                    in different european cities and countries:
+                    Dublin, Birmingham, London, Berlin and finally Madrid, my home.
+                    I have been very lucky to find a good bunch of people
+                    that tought and inspire me in many values and skils: teamwork,
+                    fellowship,concentration, eficiency, efectivity, not underestimate,
+                    be respectful, diversity ...
                   </p>
                   <p>
-                    Pellentesque tincidunt neque a mattis tempor. Suspendisse potenti.
-                    Sed sollicitudin non nisl ut faucibus. Cras laoreet auctor pulvinar.
-                    Maecenas eu leo lacus. Quisque gravida, nulla sed aliquet maximus,
-                    lorem elit suscipit dolor, sit amet volutpat ante orci tempor libero.
-                    Aenean semper, diam eget lacinia volutpat, leo metus elementum quam,
-                    sagittis eleifend elit elit vitae nulla.
+                    I would love to communicate and inspire others at least a little bit of
+                    what i gotfrom those positive experiences.
+                    Nevertheless i keep on learning and taking inspiration, values and skills
+                    from others to share it around.
+                    It can be coding, managing projects, writing documentation, being reviewed and
+                    reviewing others code, building a team, thinking about technical
+                    solution for a problem.
+                    That is my way of contributing.
                   </p>
                   <p>
-                    Aliquam ultrices vestibulum lacinia. Nunc sodales iaculis dolor id gravida.
-                    Nulla facilisi. Morbi congue maximus nulla, quis dapibus lectus tempus mollis.
-                    In id efficitur enim, eu eleifend nulla. Aliquam ac condimentum turpis.
-                    Nulla tristique tempor libero.
-                  </p>
-                  <p>
-                    Etiam vulputate ante sit amet tortor aliquam vehicula.
-                    Ut nibh tortor, posuere quis neque non, fringilla tincidunt orci.
-                    Vivamus lacus nisl, dignissim sed luctus vel, molestie quis neque.
-                    Praesent ultrices in nulla et dapibus. Phasellus facilisis convallis vehicula.
-                    Cras et odio vitae eros efficitur hendrerit eget sit amet nunc.
-                    Sed at condimentum ex.
+                    In the past i have jumped from one language to other
+                    (PHP, javascript, objective-C, python, java) to learnfrom them.
+                    Now i am currently more focus in learning new concepts and
+                    how things work inside ( browsers, event loop, mobile client,
+                    cloud, app and webapp life cycle, leadership ).
+                    In the future i would like to develop my skills to a level
+                    of solving many more kind of known problems and why not
+                    building a top performance team where a play a major role.
                   </p>
                 </div>
             </div>
