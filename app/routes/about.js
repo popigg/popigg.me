8d1e8a4298bf8377e487abd36a462107a990c36a
popigg <popigg@gmail.com>
2016-10-10 00:24:04 +0200

    fix some content issues

--- app/routes/about.js
+++ app/routes/about.js
@@ -6,43 +6,41 @@ class About extends React.Component {
     render() {
         return (
             <div>
-                <img src={imgSrc} className="row col-xs-10" />
-                <PageTitle title="about" overlineColor='yellow'/>
+                <img src="http://random.bunny-comic.com/strips/150709.gif" className="center-xs" />
+                <PageTitle title="about" overlineColor='orange'/>
                 <div className="about__content start-xs">
 
                   <p>
                     I like technology very much, and i have freely decided to dedicate
-                    my life to it. Sometimes is a world where you are avocated to change or
+                    my life to it. Sometimes is not easy, you are avocated to change or
                     mutate your skills, work on your free time, understand complexity
                     you cannot put in your mind in a language is not you mother tongue.
-                    It is not easy.
-                    Since around 10 years i found my self very motivated and try to build something
+                    Last 10 years i found my self very motivated and try to build something
                     to feel proud of.
-                    I want to build things that change and improve in something the world we live.
-                    It makes me feel very positive, happy and glad that technology allows me this.
+                    I want to build things that change and improve in some way the world we live.
+                    It makes me feel very positive, happy and glad that technology allows me to try.
                   </p>
                   <p>
                     After sharing experiences with many people,
                     in different european cities and countries:
                     Dublin, Birmingham, London, Berlin and finally Madrid, my home.
                     I have been very lucky to find a good bunch of people
-                    that tought and inspire me in many values and skils: teamwork,
-                    fellowship,concentration, eficiency, efectivity, not underestimate,
-                    be respectful, diversity ...
+                    that tought and inspire me with many values and skils: focus and flow time,
+                    teamwork, fellowship, eficiency, patience, different mindsets, diversity ...
                   </p>
                   <p>
                     I would love to communicate and inspire others at least a little bit of
-                    what i gotfrom those positive experiences.
-                    Nevertheless i keep on learning and taking inspiration, values and skills
-                    from others to share it around.
+                    what i took from others.
+                    Nevertheless i keep on learning and taking inspiration and skills
+                    to share.
                     It can be coding, managing projects, writing documentation, being reviewed and
                     reviewing others code, building a team, thinking about technical
                     solution for a problem.
                     That is my way of contributing.
                   </p>
                   <p>
-                    In the past i have jumped from one language to other
-                    (PHP, javascript, objective-C, python, java) to learnfrom them.
+                    In the past i have played with web, mobile and cloud architechtures.
+                    In a variety of languages but more focus on javascript and PHP.
                     Now i am currently more focus in learning new concepts and
                     how things work inside ( browsers, event loop, mobile client,
                     cloud, app and webapp life cycle, leadership ).
